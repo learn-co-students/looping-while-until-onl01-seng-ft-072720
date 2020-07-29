a26c9551d20846e38e61bf2ee7fd36ca65845c31
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-07-29 05:22:00 +0000

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,12 @@
-def using_until
+def using_until()
   levitation_force = 6
+  #until our levitation force equals 10
+  until levitation_force == 10
+  #puts the desired phrase, "Wingardium Leviosa"
+  puts "Wingardium Leviosa"
+  #increment your levitation force by 1
+  levitation_force += 1
   #your code here
-    
+  end  
 end
 
--- lib/while.rb
+++ lib/while.rb
@@ -1,6 +1,11 @@
-def using_while
+def using_while()
+#we should increment our levitation force by 1
   levitation_force = 6
-	
+#while our levitation force is less than 10
+   while levitation_force < 10
+  levitation_force += 1
+    puts "Wingardium Leviosa"
+  end
   #your code here
 end
 
